b0dbd339d545610e9b36b84c3f0ddfd0e7ed4e28
Stefan Hundhammer <shundhammer@suse.de>
2024-12-04 14:40:45 +0100

    Fixed build

--- src/YQIconPool.cc
+++ src/YQIconPool.cc
@@ -85,7 +85,7 @@ YQIconPool::~YQIconPool()
 QPixmap
 YQIconPool::cachedIcon( const QString icon_name, const bool enabled )
 {
-    Q_INIT_RESOURCE(qt_pkg_icons);
+    Q_INIT_RESOURCE( icons );
     QPixmap iconPixmap = _iconCache[ icon_name + enabled ];
 
     if ( !iconPixmap )
