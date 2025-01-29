0aa2d90f0dbf441e59a7c6a6a89d45d1e0c8aa23
Stefan Hundhammer <shundhammer@suse.de>
2025-01-29 11:32:32 +0100

    Coding style

--- src/YQZypp.h
+++ src/YQZypp.h
@@ -21,6 +21,7 @@
 #include <set>
 #include <zypp/ui/Status.h>
 #include <zypp/ui/Selectable.h>
+#include <zypp/RepoInfo.h>
 #include <zypp/ResObject.h>
 #include <zypp/Package.h>
 #include <zypp/Pattern.h>
@@ -46,57 +47,59 @@ using zypp::ui::S_NoInst;
 // Typedefs to make those nested namespaces human-readable
 //
 
-typedef zypp::ui::Status			ZyppStatus;
-typedef zypp::ui::Selectable::Ptr		ZyppSel;
+typedef zypp::ui::Status                        ZyppStatus;
+typedef zypp::ui::Selectable::Ptr               ZyppSel;
+typedef zypp::RepoInfo                          ZyppRepoInfo;
 typedef zypp::Resolvable::constPtr              ZyppRes;
-typedef zypp::ResObject::constPtr		ZyppObj;
-typedef zypp::Package::constPtr			ZyppPkg;
-typedef zypp::Pattern::constPtr			ZyppPattern;
-typedef zypp::Patch::constPtr			ZyppPatch;
-typedef zypp::Product::constPtr			ZyppProduct;
-typedef zypp::PoolItem       			ZyppPoolItem;
+typedef zypp::ResObject::constPtr               ZyppObj;
+typedef zypp::Package::constPtr                 ZyppPkg;
+typedef zypp::Pattern::constPtr                 ZyppPattern;
+typedef zypp::Patch::constPtr                   ZyppPatch;
+typedef zypp::Product::constPtr                 ZyppProduct;
+typedef zypp::PoolItem                          ZyppPoolItem;
 
 
-typedef zypp::ResPoolProxy			ZyppPool;
-typedef zypp::ResPoolProxy::const_iterator	ZyppPoolIterator;
-typedef zypp::ResPoolProxy::repository_iterator	ZyppRepositoryIterator;
+typedef zypp::ResPoolProxy                      ZyppPool;
+typedef zypp::ResPoolProxy::const_iterator      ZyppPoolIterator;
+typedef zypp::ResPoolProxy::repository_iterator ZyppRepositoryIterator;
 
-inline ZyppPool		zyppPool()		{ return zypp::getZYpp()->poolProxy();	}
+inline ZyppPool         zyppPool()              { return zypp::getZYpp()->poolProxy();  }
 
-template<class T> ZyppPoolIterator zyppBegin()	{ return zyppPool().byKindBegin<T>();	}
-template<class T> ZyppPoolIterator zyppEnd()	{ return zyppPool().byKindEnd<T>();	}
+template<class T> ZyppPoolIterator zyppBegin()  { return zyppPool().byKindBegin<T>();   }
+template<class T> ZyppPoolIterator zyppEnd()    { return zyppPool().byKindEnd<T>();     }
 
-inline ZyppPoolIterator zyppPkgBegin()		{ return zyppBegin<zypp::Package>();	}
-inline ZyppPoolIterator zyppPkgEnd()		{ return zyppEnd<zypp::Package>();	}
+inline ZyppPoolIterator zyppPkgBegin()          { return zyppBegin<zypp::Package>();    }
+inline ZyppPoolIterator zyppPkgEnd()            { return zyppEnd  <zypp::Package>();    }
 
-inline ZyppPoolIterator zyppPatternsBegin()	{ return zyppBegin<zypp::Pattern>();	}
-inline ZyppPoolIterator zyppPatternsEnd()	{ return zyppEnd<zypp::Pattern>();	}
+inline ZyppPoolIterator zyppPatternsBegin()     { return zyppBegin<zypp::Pattern>();    }
+inline ZyppPoolIterator zyppPatternsEnd()       { return zyppEnd  <zypp::Pattern>();    }
 
-inline ZyppPoolIterator zyppPatchesBegin()	{ return zyppBegin<zypp::Patch>();	}
-inline ZyppPoolIterator zyppPatchesEnd()	{ return zyppEnd<zypp::Patch>();	}
+inline ZyppPoolIterator zyppPatchesBegin()      { return zyppBegin<zypp::Patch>();      }
+inline ZyppPoolIterator zyppPatchesEnd()        { return zyppEnd  <zypp::Patch>();      }
 
-inline ZyppPoolIterator zyppProductsBegin()	{ return zyppBegin<zypp::Product>();	}
-inline ZyppPoolIterator zyppProductsEnd()	{ return zyppEnd<zypp::Product>();	}
+inline ZyppPoolIterator zyppProductsBegin()     { return zyppBegin<zypp::Product>();    }
+inline ZyppPoolIterator zyppProductsEnd()       { return zyppEnd  <zypp::Product>();    }
 
 inline ZyppRepositoryIterator ZyppRepositoriesBegin() { return zyppPool().knownRepositoriesBegin(); }
 inline ZyppRepositoryIterator ZyppRepositoriesEnd()   { return zyppPool().knownRepositoriesEnd();   }
 
-inline ZyppPkg		tryCastToZyppPkg( ZyppObj zyppObj )
+
+inline ZyppPkg tryCastToZyppPkg( ZyppObj zyppObj )
 {
     return zypp::dynamic_pointer_cast<const zypp::Package>( zyppObj );
 }
 
-inline ZyppPattern 	tryCastToZyppPattern( ZyppObj zyppObj )
+inline ZyppPattern tryCastToZyppPattern( ZyppObj zyppObj )
 {
     return zypp::dynamic_pointer_cast<const zypp::Pattern>( zyppObj );
 }
 
-inline ZyppPatch	tryCastToZyppPatch( ZyppObj zyppObj )
+inline ZyppPatch tryCastToZyppPatch( ZyppObj zyppObj )
 {
     return zypp::dynamic_pointer_cast<const zypp::Patch>( zyppObj );
 }
 
-inline ZyppProduct	tryCastToZyppProduct( ZyppObj zyppObj )
+inline ZyppProduct tryCastToZyppProduct( ZyppObj zyppObj )
 {
     return zypp::dynamic_pointer_cast<const zypp::Product>( zyppObj );
 }
